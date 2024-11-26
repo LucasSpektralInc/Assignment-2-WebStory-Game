73d6b354876fe6b9e3c5b30bc41bff24002fdc98
LucasSpektralInc <145656071+LucasSpektralInc@users.noreply.github.com>
2024-11-26 23:49:00 +0000

    All files already in Version 5
    Completed "game.html"

---
+++ Shadow Protocol-Neon Vault/Solution/Shadow Protocol_ Neon Vault/game_script.js
@@ -0,0 +1,21 @@
+// var buttons = document.getElementsById("container");
+// var button = document.createElement("button");
+//
+// Walk_Away: {
+//     text: 'You walk away from the tree and start walking back to your house. You see that your house is broken into',
+//     options: [
+//       { text: 'Run Straight Inside', nextStep: 'Run_Straight_Inside' },
+//       { text: 'Hide In Bush', nextStep: 'Hide_In_Bush' }
+//     ]
+//   },
+//   Hide_In_Bush: {
+//     text: 'You get tapped on the shoulder. You turn around and have a gas-mask put over your face. You wake up strapped to a chair. A crazy person walks up to you, you can see his rib cage and he is hooked up to an IV drip. He is carrying shears. He slaps you in the face and cuts your fingers off',
+//     options: [
+//       { text: 'Continue', nextStep: 'GameOver' }
+//     ]
+//   },
+//   Run_Straight_Inside: {
+//     text: 'You run inside your home to realise that it has been ransacked. The only thing that seems to be missing is a metal box that has been sitting on your shelf for years',
+//     options: [
+//       { text: 'Go Back To The Tree', nextStep: 'beginning' },
+//       { text: 'Stay Home', nextStep: 'Stay_Home' }
